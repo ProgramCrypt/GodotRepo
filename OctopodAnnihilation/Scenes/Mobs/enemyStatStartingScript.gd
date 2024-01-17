1b04ad939800e329499f74e006bb42e76ff287a2
ProgramCrypt <60086361+ProgramCrypt@users.noreply.github.com>
2024-01-16 17:54:43 -0800

    Starting Stats
    Added character base stats and single enemy stats

---
+++ OctopodAnnihilation/Scenes/Mobs/enemyStatStartingScript.gd
@@ -0,0 +1,20 @@
+extends Resource
+
+class_name mobStartingStats
+
+@export var character_name : String=''
+
+@export var maxHealth : int
+@export var currentHealth : int
+@export var speed : int
+@export var strength : int
+@export var rateOfFire : int
+@export var gunAccuracy : int
+@export var ballisticResistance : int
+@export var laserResistance : int
+@export var plasmaResistance : int
+@export var bleedingResistance : int
+@export var fireResistance : int
+@export var explosionResistance : int
+@export var slowResistance : int
+@export var stunResistance : int
---
+++ OctopodAnnihilation/Scenes/PCs/StartingScripts.gd
@@ -0,0 +1,22 @@
+extends Resource
+
+class_name StartingStats
+
+@export var character_name : String='SuperSoldier'
+
+@export var maxHealth : int
+@export var currentHealth : int
+@export var maxEnergy : int
+@export var currentEnergy : int
+@export var speed : int
+@export var strength : int
+@export var rateOfFire : int
+@export var gunAccuracy : int
+@export var ballisticResistance : int
+@export var laserResistance : int
+@export var plasmaResistance : int
+@export var bleedingResistance : int
+@export var fireResistance : int
+@export var explosionResistance : int
+@export var slowResistance : int
+@export var stunResistance : int
